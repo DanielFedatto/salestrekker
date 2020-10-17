0e14fa1994cbfe5ad1e1d70863e0bda0cae11f55
danielfedatto <daniel.fedatto@penseavanti.com.br>
2020-10-17 02:59:11 -0300

    :sparkles: Add logo image from source

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import LogoWhite from '../../images/logo-white.png'
+import LogoDark from '../../images/logo-dark.png'
 
 function Logo() {
     return (
         <figure className="logo">
             <a href="#" title="Salestrekker">
-                <img src="./src/images/logo-white.png"/>
+                <img src={LogoWhite}/>
             </a>
         </figure>
     )
