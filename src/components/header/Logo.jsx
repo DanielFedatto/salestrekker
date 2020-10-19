1c1c81bd26557a1b5d8ab5e47d3a4085066ad586
danielfedatto <daniel.fedatto@penseavanti.com.br>
2020-10-19 06:11:36 -0300

    :boom: Changing image logo structure

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -1,14 +1,10 @@
 import React, { Component } from 'react'
-import LogoWhite from '../../images/logo-white.png'
-import LogoDark from '../../images/logo-dark.png'
 
 function Logo() {
     return (
-        <figure className="logo">
-            <a href="#" title="Salestrekker">
-                <img src={LogoWhite}/>
-            </a>
-        </figure>
+        <a href="#" title="Salestrekker">
+            <figure className="logo" />
+        </a>
     )
 }
 
